083f37d3a645656d2a4e9681549673112842a516
enricogoerlitz <rico.goerlitz@gmail.com>
2022-04-25 21:46:29 +0200

    heroku4

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -11,7 +11,6 @@
 
 <script>
 import axios from "axios";
-
 import AuthJWTCookie from "@/authentication/classes/AuthJWTCookie";
 import { createAPIRoute } from "../authentication/modules/backend";
 
